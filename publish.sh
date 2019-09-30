e49e105b4220727a828ca7cd4c3d99c0ae71f8c9
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2019-09-30 16:50:20 +0200

    chore(CI): fix source path

--- publish.sh
+++ publish.sh
@@ -10,9 +10,9 @@ mkdir /tmp/build && cd /tmp/build && cmake "${SOURCE_DIR}" -DCMAKE_INSTALL_PREFI
 cd "${SOURCE_DIR}"/dist && tar cfv "fumi_tools_${VERSION}_static_linux_x86_64.tar.gz" bin
 
 # build source tgz that includes version.hpp generated previously
-mkdir /tmp/"${SOURCE_DIR}$$"
-cp -r "${SOURCE_DIR}" /tmp/"${SOURCE_DIR}$$/fumi_tools-${VERSION}"
-find /tmp/"${SOURCE_DIR}$$" -type d -name '.git' | xargs -n1 rm -rf
-cd /tmp/"${SOURCE_DIR}$$"
+mkdir /tmp/$$
+cp -r "${SOURCE_DIR}" /tmp/"$$/fumi_tools-${VERSION}"
+find /tmp/$$ -type d -name '.git' | xargs -n1 rm -rf
+cd /tmp/$$
 tar cfv fumi_tools-${VERSION}.tar.gz fumi_tools-${VERSION}
 mv fumi_tools-${VERSION}.tar.gz "${SOURCE_DIR}"/dist
