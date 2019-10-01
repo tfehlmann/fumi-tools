13f13300be8a9b32dc57a7534219b14a6ff9cc0f
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2019-10-01 20:36:18 +0200

    feat(usability): fumi_tools now also works if it's not in the PATH

--- publish.sh
+++ publish.sh
@@ -7,12 +7,12 @@ VERSION="$2"
 
 # build static binaries
 mkdir /tmp/build && cd /tmp/build && cmake "${SOURCE_DIR}" -DCMAKE_INSTALL_PREFIX="${SOURCE_DIR}"/dist -DBUILD_SHARED_LIBS=OFF && make -j4 install
-cd "${SOURCE_DIR}"/dist && tar cfv "fumi_tools_${VERSION}_static_linux_x86_64.tar.gz" bin
+cd "${SOURCE_DIR}"/dist && tar czfv "fumi_tools_${VERSION}_static_linux_x86_64.tar.gz" bin
 
 # build source tgz that includes version.hpp generated previously
 mkdir /tmp/$$
 cp -r "${SOURCE_DIR}" /tmp/"$$/fumi_tools-${VERSION}"
 find /tmp/$$ -type d -name '.git' | xargs -n1 rm -rf
 cd /tmp/$$
-tar cfv fumi_tools-${VERSION}.tar.gz fumi_tools-${VERSION}
+tar czfv fumi_tools-${VERSION}.tar.gz fumi_tools-${VERSION}
 mv fumi_tools-${VERSION}.tar.gz "${SOURCE_DIR}"/dist
