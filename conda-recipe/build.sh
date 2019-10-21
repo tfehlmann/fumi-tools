18d9f025003ab893ffe74424aff9f11033f5cf4a
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2019-10-22 00:13:34 +0200

    chore(CI): add automatic conda package deployment

---
+++ conda-recipe/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # Abort on error.
+
+git submodule init
+git submodule update
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_GENERIC=On -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
+
+make -j ${CPU_COUNT} ${VERBOSE_AT}
+make install
