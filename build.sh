65ef53b7c26748c0fbcea1241ce84add4942bb81
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2022-03-03 02:35:14 +0100

    build: update bundeled dist folder

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ mkdir /tmp/build && cd /tmp/build
 ${CMAKE} -S "${SOURCE_DIR}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${SOURCE_DIR}/dist/${PLATFORM}" -DBUILD_SHARED_LIBS=OFF -DBUILD_GENERIC=ON "${CMAKE_ARGS}"
 make -j4 install
 
-cd "${SOURCE_DIR}/dist/${PLATFORM}" && tar czfv "fumi_tools_${VERSION}_static_${PLATFORM}.tar.gz" bin && cd .. && rm -rf "${PLATFORM}"
+cd "${SOURCE_DIR}/dist/${PLATFORM}" && tar czfv "../fumi_tools_${VERSION}_static_${PLATFORM}.tar.gz" bin && cd .. && rm -rf "${PLATFORM}"
 
 # remove the tag here so that the tag will only be created if all builds pass
 curl --silent --request DELETE --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/repository/tags/${VERSION}"
